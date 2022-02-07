d6b5e7458b1474a098c8f4a5f6d8ce5af2f1d7ae
Elias Horner <elias.horner@student.htldornbirn.at>
2022-02-07 14:22:13 +0100

    add calls in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
+import 'package:f_logs/constants/db_constants.dart';
+import 'package:f_logs/model/flog/flog.dart';
+import 'package:f_logs/model/flog/flog_config.dart';
+import 'package:f_logs/model/flog/log_level.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:sembast/sembast.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setPreferredOrientations();
+  await dotenv.load(fileName: ".env");
+  setupLogging();
+  await setupSSL();
+
   runApp(MyApp());
 }
 
@@ -25,6 +40,61 @@ class MyApp extends StatelessWidget {
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
+}
+
+void setupLogging() {
+  LogsConfig logsConfig = FLog.getDefaultConfigurations();
+
+  bool logAll = dotenv.env["LOG_ALL"] == "true" || dotenv.env["LOG_ALL"] == "1";
+  if(! logAll) {
+    logsConfig.activeLogLevel = LogLevel.WARNING;
+  }
+
+  FLog.applyConfigurations(logsConfig);
+
+  // delete logs older than 48h
+  FLog.deleteAllLogsByFilter(filters: [
+    Filter.lessThan(DBConstants.FIELD_TIME_IN_MILLIS,
+        DateTime.now().millisecondsSinceEpoch - 72 * 60 * 60 * 1000)
+  ]);
+}
+
+Future<SecurityContext> setupSSL() async {
+  ByteData data = await PlatformAssetBundle().load('assets/ca/lets-encrypt-r3.pem');
+
+  SecurityContext securityContext = SecurityContext.defaultContext;
+  try {
+    securityContext.setTrustedCertificatesBytes(data.buffer.asUint8List());
+  } on TlsException catch (e, st) {
+    if (e.osError?.message != null &&
+        e.osError?.message.contains('CERT_ALREADY_IN_HASH_TABLE') == true) {
+      FLog.logThis(
+          text: 'createHttpClient() - cert already trusted! Skipping.',
+          type: LogLevel.WARNING,
+          exception: e,
+          stacktrace: st
+      );
+    } else {
+      FLog.logThis(
+          text: 'createHttpClient() - exception',
+          type: LogLevel.ERROR,
+          exception: e,
+          stacktrace: st
+      );
+    }
+  }
+
+  return securityContext;
+
+}
+
+Future<void> setPreferredOrientations() {
+  return SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.landscapeRight,
+    DeviceOrientation.landscapeLeft,
+  ]);
 }
 
 class MyHomePage extends StatefulWidget {
